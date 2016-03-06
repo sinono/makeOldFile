8c034e5323b2a2769021bca766130fc8cc1e7fe4
sinono <sinono.wp@gmail.com>
2016-03-06 11:06:59 +0900

    first

---
+++ makeOldFile.ps1
@@ -0,0 +1,21 @@
+<#
+HTMLファイルのoldファイルを作るスクリプト
+#>
+
+Get-ChildItem
+
+#今日の日付の文字列
+$txtYmd = Get-Date -Format "yyMMdd"
+
+#htmlファイルだけを取得
+$html = Get-ChildItem *.html
+
+foreach($f in $html) {
+    
+    #現在のファイル名（拡張子を除く）
+    $currentFileName = $f.name.substring(0,$f.name.length -5 )
+    
+    #oldファイルを作成
+    copy-item $f ($currentFileName + "_old" + $txtYmd + ".html")
+}
+
