2237cd7b1f0acf83fc8667c6722ef805eb6344f3
sinono <sinono.wp@gmail.com>
2016-03-10 08:10:09 +0900

    ユーザは、コピーする対象のファイルの拡張子をコマンドラインで指定する

--- makeOldFile.ps1
+++ makeOldFile.ps1
@@ -1,21 +1,37 @@
-<#
-HTMLファイルのoldファイルを作るスクリプト
-#>
+#指定された拡張子のファイルのoldファイルを作る
+function makeOldFile ($ext) {
 
-Get-ChildItem
+    #今日の日付の文字列
+    $txtYmd = Get-Date -Format "yyMMdd"
 
-#今日の日付の文字列
-$txtYmd = Get-Date -Format "yyMMdd"
-
-#htmlファイルだけを取得
-$html = Get-ChildItem *.html
-
-foreach($f in $html) {
+    #指定された拡張子のファイルだけを取得
+    $match_ext = "*." + $ext
+    $files = Get-ChildItem  $match_ext
+    #$files = Get-ChildItem  "*.html"
     
-    #現在のファイル名（拡張子を除く）
-    $currentFileName = $f.name.substring(0,$f.name.length -5 )
-    
-    #oldファイルを作成
-    copy-item $f ($currentFileName + "_old" + $txtYmd + ".html")
+    #指定のファイルが存在しない場合、処理は行わない
+    if ($files -eq $null) {
+      Write-Host "no file:" $match_ext
+      Write-Host "end"
+      exit
+    }else {
+        Write-Host "start copy file:" $match_ext
+    }
+
+    foreach($f in $files) {
+          
+        #現在のファイル名（拡張子を除く）
+        $currentFileName = $f.name.substring(0,$f.name.length -5 )
+        
+        #oldファイルを作成
+        copy-item $f ($currentFileName + "_old" + $txtYmd + "." + $ext)
+    }
+
 }
 
+    Write-Host "Please type file filename extension :)"
+    Write-Host "[KAKUCHO-SHI] ex. `"html`" `"css`" "-NoNewLine
+
+    $extension = Read-Host
+
+makeOldFile($extension)
