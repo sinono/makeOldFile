97c53383efc1f40b2cc8c3492b40dc8ba061245d
sinono <sinono.wp@gmail.com>
2016-04-01 18:31:18 +0900

    change kaomoji

--- makeOldFile.ps1
+++ makeOldFile.ps1
@@ -1,13 +1,12 @@
 #指定された拡張子のファイルのoldファイルを作る
 function makeOldFile ($ext) {
-
+    
     #今日の日付の文字列
     $txtYmd = Get-Date -Format "yyMMdd"
 
     #指定された拡張子のファイルだけを取得
     $match_ext = "*." + $ext
     $files = Get-ChildItem  $match_ext
-    #$files = Get-ChildItem  "*.html"
     
     #指定のファイルが存在しない場合、処理は行わない
     if ($files -eq $null) {
@@ -29,7 +28,7 @@ function makeOldFile ($ext) {
 
 }
 
-    Write-Host "Please type file filename extension :)"
+    Write-Host "Please type file filename extension (^-^)"
     Write-Host "[KAKUCHO-SHI] ex. `"html`" `"css`" "-NoNewLine
 
     $extension = Read-Host
